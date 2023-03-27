27c4da6d11cf92dbd4e6adb1ef8e2aaad658aea5
Arne <arne@ptr-d6drin58tp7jysrhhra.18120a2.ip6.access.telenet.be>
2023-03-27 21:35:56 +0200

    updated scripts after testing

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -3,14 +3,12 @@ return {
 	enable_tab_bar = false,
 	window_decorations = "RESIZE",
 	color_scheme = "seoulbones_dark",
-	-- color_scheme = "idleToes",
-	front_end = "WebGpu",
 	colors = {
 		background = "#202020",
 		cursor_bg = "#cccccc",
 	},
 
-	font_size = 22.0,
+	font_size = 12.0,
 	window_close_confirmation = "NeverPrompt",
 	adjust_window_size_when_changing_font_size = false,
 	font = wezterm.font("Fira Code"),
