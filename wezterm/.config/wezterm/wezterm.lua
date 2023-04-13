f7e0dacd37adae751ebb94c79a9dcb10e82490c3
arnevm123 <arnevanmaele@gmail.com>
2023-04-13 22:18:19 +0200

    jkl

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -13,7 +13,7 @@ return {
 	window_close_confirmation = "NeverPrompt",
 	adjust_window_size_when_changing_font_size = false,
 	font = wezterm.font("Fira Code"),
-	harfbuzz_features = { "calt=1", "clig=1", "liga=1" },
+	harfbuzz_features = { "calt=0", "clig=0", "liga=0" },
 	font_rules = {
 		{
 			italic = true,
