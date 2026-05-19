3e1902a988a60b69ac6e34059ff98bcd5bd4e094
Arne Van Maele <arnevanmaele@gmail.com>
2026-05-19 14:10:48 +0200

    Last Sync: 2026-05-19 14:10 (Manual commit)

--- nvim/.config/nvim/plugin/file-utils.lua
+++ nvim/.config/nvim/plugin/file-utils.lua
@@ -1,3 +1,20 @@
 vim.pack.add({ "https://github.com/wsdjeg/vim-fetch" }) -- :e file:80
 vim.pack.add({ "https://github.com/lambdalisue/vim-suda" }) -- save with sudo
 vim.pack.add({ "https://github.com/tpope/vim-eunuch" }) -- :Delete, :Move, :Copy, :Mkdir, etc.
+vim.pack.add({ "https://github.com/tpope/vim-projectionist" }) -- :A to go to alternate file
+
+vim.g.projectionist_heuristics = {
+	["go.mod"] = {
+		["*.go"] = {
+			alternate = "{}_test.go",
+			type = "source",
+		},
+		["*_test.go"] = {
+			alternate = "{}.go",
+			type = "test",
+		},
+	},
+}
+
+local map = require("keymaps").map
+map("n", "<leader>ot", "<cmd>A<CR>", "Toggle alternate file")
